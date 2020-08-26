9fdff96df9e1597d8f147d6596c3c38ce97a2524
patrick <patrick204nqh@gmail.com>
2020-08-26 10:56:40 +0700

    connect MongoDB

---
+++ README.md
@@ -0,0 +1,6 @@
+# Patrick's Project (ReactJS & GraphQL)
+
+1. Source videos:
+
+- Server:
+  > https://www.youtube.com/watch?v=71-CtIcmDJQ&list=PLMhAeHCz8S3_CTiWMQhL6YxX7vZ7z84Zo&index=2
---
+++ index.js
@@ -0,0 +1,33 @@
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+const { MONGODB } = require('./config');
+
+const typeDefs = gql`
+  type Query {
+    sayHi: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    sayHi: () => 'hello, world'
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB connected');
+    return server.listen({ port: 5000 })
+  })
+  .then(res => {
+    console.log(`server running at ${res.url}`);
+  })
+  .catch(err => console.log(err));
