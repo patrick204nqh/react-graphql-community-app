6e12beaa106267be34ab7ba44b7d31b8dc6360b5
patrick <patrick204nqh@gmail.com>
2020-09-03 01:22:45 +0700

    show error list

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -3,7 +3,8 @@ import { Form, Button } from 'semantic-ui-react';
 import { useMutation } from '@apollo/react-hooks'
 import gql from 'graphql-tag';
 
-function Register() {
+function Register(props) {
+  const [errors, setErrors] = useState({});
   const [values, setValues] = useState({
     username: '',
     email: '',
@@ -16,8 +17,13 @@ function Register() {
   }
 
   const [addUser, { loading }] = useMutation(REGISTER_USER, {
-    update(proxy, result) {
-      console.log(result)
+    update(_, result) {
+      console.log(result);
+      props.history.push('/');
+    },
+    onError(err) {
+      console.log(err.graphQLErrors[0].extensions.exception.errors);
+      setErrors(err.graphQLErrors[0].extensions.exception.errors);
     },
     variables: values
   })
@@ -37,6 +43,7 @@ function Register() {
           name="username"
           type="text"
           value={values.username}
+          error={errors.username ? true : false}
           onChange={onChange}
         />
         <Form.Input
@@ -45,6 +52,7 @@ function Register() {
           name="email"
           type="email"
           value={values.email}
+          error={errors.email ? true : false}
           onChange={onChange}
         />
         <Form.Input
@@ -53,6 +61,7 @@ function Register() {
           name="password"
           type="password"
           value={values.password}
+          error={errors.password ? true : false}
           onChange={onChange}
         />
         <Form.Input
@@ -61,12 +70,22 @@ function Register() {
           name="confirmPassword"
           type="password"
           value={values.confirmPassword}
+          error={errors.confirmPassword ? true : false}
           onChange={onChange}
         />
         <Button type="submit" primary>
           Register
         </Button>
       </Form>
+      {Object.keys(errors).length > 0 && (
+        <div className="ui error message">
+          <ul className="list">
+            {Object.values(errors).map(value => (
+              <li key={value}>{value}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
