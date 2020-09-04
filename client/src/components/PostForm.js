28103bb027328006765cd040355fb39104cf22f0
patrick <patrick204nqh@gmail.com>
2020-09-04 14:47:44 +0700

    refresh data when create post

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Form, Button } from 'semantic-ui-react';
-import gql from 'graphql-tag';
 import { useMutation } from '@apollo/react-hooks';
-
 import { useForm } from '../util/hooks';
+
+import {
+  CREATE_POST_MUTATION,
+  FETCH_POSTS_QUERY
+} from '../util/graphql';
 
 function PostForm() {
 
@@ -13,8 +16,12 @@ function PostForm() {
 
   const [createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
     variables: values,
-    update(_, result) {
-      console.log(result)
+    update(proxy, result) {
+      const data = proxy.readQuery({
+        query: FETCH_POSTS_QUERY
+      })
+      data.getPosts = [result.data.createPost, ...data.getPosts];
+      proxy.writeQuery({ query: FETCH_POSTS_QUERY, data });
       values.body = '';
     }
   });
@@ -38,21 +45,5 @@ function PostForm() {
     </Form>
   )
 }
-
-const CREATE_POST_MUTATION = gql`
-  mutation createPost($body: String!) {
-    createPost(body: $body){
-      id body createdAt username
-      likes{
-        id username createdAt
-      }
-      likeCount
-      comments{
-        id body username createdAt
-      }
-      commentCount
-    }
-  }
-`;
 
 export default PostForm;
---
+++ client/src/util/graphql.js
@@ -0,0 +1,32 @@
+import gql from 'graphql-tag';
+
+export const CREATE_POST_MUTATION = gql`
+mutation createPost($body: String!) {
+  createPost(body: $body){
+    id body createdAt username
+    likes{
+      id username createdAt
+    }
+    likeCount
+    comments{
+      id body username createdAt
+    }
+    commentCount
+  }
+}
+`;
+
+export const FETCH_POSTS_QUERY = gql`
+{
+  getPosts{
+    id body createdAt username likeCount
+    likes{
+      username
+    }
+    commentCount
+    comments{
+      id username createdAt body
+    }
+  }
+}
+`;
